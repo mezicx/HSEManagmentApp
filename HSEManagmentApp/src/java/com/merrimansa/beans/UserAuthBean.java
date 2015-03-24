ebb4c62eb6664eeeccbc910e2eef15036a0a0ea3
Steve <Steve@Steves_Laptop>
2015-03-24 07:09:24 +0000

    Pushing Security -mega problems see blog

---
+++ HSEManagmentApp/src/java/com/merrimansa/beans/UserAuthBean.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.merrimansa.beans;
+
+import java.io.IOException;
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.PostConstruct;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import org.apache.commons.codec.digest.DigestUtils;
+
+/**
+ *
+ * @author Steve
+ */
+@Named(value = "userAuthBean")
+@SessionScoped
+public class UserAuthBean implements Serializable {
+
+    /**
+     * Creates a new instance of UserAuthBean
+     */
+    public UserAuthBean() {
+    }
+    
+    private String UserName,Password;
+    private String originalURL;
+
+    public String getUserName() {
+        return UserName;
+    }
+
+    public void setUserName(String UserName) {
+        this.UserName = UserName;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String Password) {
+        this.Password = Password;
+    }
+    
+        
+    @PostConstruct
+    public void init() {
+        ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
+        originalURL = (String) externalContext.getRequestMap().get(RequestDispatcher.FORWARD_REQUEST_URI);
+
+        if (originalURL == null) {
+            originalURL = externalContext.getRequestContextPath() + "/index.xhtml";
+        } else {
+            String originalQuery = (String) externalContext.getRequestMap().get(RequestDispatcher.FORWARD_QUERY_STRING);
+
+            if (originalQuery != null) {
+                originalURL += "?" + originalQuery;
+            }
+        }
+    }
+    
+    public void login() throws IOException{
+        FacesContext context = FacesContext.getCurrentInstance();
+        HttpServletRequest request = (HttpServletRequest)context.getExternalContext().getRequest();
+        ExternalContext externalContext = context.getExternalContext();
+        try {
+            String PasswordHash = DigestUtils.sha256Hex(Password);
+            System.out.println(PasswordHash);
+            request.login(UserName, Password);
+            externalContext.redirect(originalURL);
+        } catch (ServletException ex) {
+            Logger.getLogger(UserAuthBean.class.getName()).log(Level.SEVERE, null, ex);
+            String error = externalContext.getRequestContextPath() + "/loginError.xhtml";
+             externalContext.redirect(error);
+        }
+         
+    }
+    
+}
