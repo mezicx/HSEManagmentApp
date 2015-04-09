01189d20e1da06105ab6951d15c49bf682b65470
Steve <Steve@Steves_Laptop>
2015-04-09 07:17:05 +0100

    menuitem fixed now trying to get data from databtable

--- HSEManagmentApp/src/java/com/merrimansa/beans/AssignAssessmentBean.java
+++ HSEManagmentApp/src/java/com/merrimansa/beans/AssignAssessmentBean.java
@@ -3,19 +3,22 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.merrimansa.beans;
 
 import com.merrimansa.businessObjects.RequiredAssessmentVO;
 import com.merrimansa.businessObjects.UserVO;
 import com.merrimansa.ejb.ProcessAssessmentManagerFacade;
 import com.merrimansa.ejb.UserManagerFacade;
+import java.io.Serializable;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.Conversation;
+import javax.enterprise.context.ConversationScoped;
 import javax.enterprise.context.Dependent;
+import javax.enterprise.context.SessionScoped;
 import javax.faces.application.FacesMessage;
-import javax.faces.component.html.HtmlDataTable;
 import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -26,44 +29,51 @@ import org.primefaces.component.datatable.DataTable;
  * @author Steve
  */
 @Named(value = "assignAssessmentBean")
-@Dependent
-public class AssignAssessmentBean {
+@ConversationScoped
+public class AssignAssessmentBean implements Serializable {
 
     /**
      * Creates a new instance of AssignAssessmentBean
-     * 
-     * 
+     *
+     *
      */
-    
     @Inject
     private ProcessAssessmentManagerFacade PAMF;
     @Inject
     private UserManagerFacade UMF;
-    
-    private String assignedUser;
+    @Inject
+    private Conversation conversation;
+
+    private int assignedUser;
     private DataTable reqAssessments;
-    
+
     public AssignAssessmentBean() {
     }
-    
-    public List<RequiredAssessmentVO> getRequiredAssessments(){
+
+    @PostConstruct
+    public void init() {
+        conversation.begin();
+    }
+
+    public List<RequiredAssessmentVO> getRequiredAssessments() {
         return PAMF.getRequiredAssessments();
     }
-    
-    public Map<String,String>getAssessorList(){
-       Map<String,String> userMap = new HashMap();
-        for(UserVO u:UMF.getActiveUsers("Assessor")){
-            userMap.put(u.getFullName(), Integer.toString(u.getUserId()));
+
+    public Map<String, Integer> getAssessorList() {
+        Map<String, Integer> userMap = new HashMap();
+        for (UserVO u : UMF.getActiveUsers("Assessor")) {
+            userMap.put(u.getFullName(), u.getUserId());
         }
-        
+
         return userMap;
     }
-    
-    
-    public void sayMessage(){
-         FacesContext context = FacesContext.getCurrentInstance();
-         System.out.println("Growl go");
-        context.addMessage(null, new FacesMessage("Successful",  "The Assessor is "+this.getAssignedUser()) );
+
+    public void sayMessage() {
+        FacesContext context = FacesContext.getCurrentInstance();
+        System.out.println("Growl go");
+        UserVO vo =(UserVO)reqAssessments.getRowData("0");
+        context.addMessage(null, new FacesMessage("Successful", "The Assessor is " + this.getAssignedUser()+ " "+vo.getFullName()));
+        conversation.end();
     }
 
     public DataTable getReqAssessments() {
@@ -74,14 +84,14 @@ public class AssignAssessmentBean {
         this.reqAssessments = reqAssessments;
     }
 
-    public String getAssignedUser() {
+    public int getAssignedUser() {
         return assignedUser;
     }
 
-    public void setAssignedUser(String assignedUser) {
+    public void setAssignedUser(int assignedUser) {
+
+        System.out.println("It has been set");
         this.assignedUser = assignedUser;
     }
-    
-    
-    
+
 }
